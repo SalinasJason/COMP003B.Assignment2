0c312974958b2ce6f185d33da6f6cb61d7ce3e47
SalinasJason <jasonsalinas770@gmail.com>
2025-03-26 16:23:43 -0700

    Added a class to the middleware to log the HTTP request and log the HTTP response

--- COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
+++ COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
@@ -2,5 +2,18 @@
 {
     public class RequestTrackerMiddleware
     {
+        private readonly RequestDelegate _next;
+
+        public RequestTrackerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Response] {context.Response.StatusCode}");
+        }
     }
 }
